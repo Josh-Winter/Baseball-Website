25b7d6efa5da96a6bd5c7fbcfcec6998df7d59d4
Josh-Winter <98134599+Josh-Winter@users.noreply.github.com>
2022-11-18 02:39:22 -0500

    Add files via upload

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,1 +1,22 @@
-
+//clock script by Josh Winter
+function currentTime() {
+    let date = new Date(); //initialize date
+    let hour = date.getHours(); //initialize hour
+    let minute = date.getMinutes(); //initialize minute
+    let second = date.getSeconds(); //initialize second
+    let period = "AM"; //declare period as AM
+    if(hour === 0){ //set midnight
+        hour = 12; //change hour value
+    }
+    if(hour > 12){  //if hours is greater than 12
+        hour = hour - 12; //reset hour
+        period = "PM"; //change period to PM
+    }
+    hour = (hour < 10) ? "0" + hour : hour; //set final hour value
+    minute = (minute < 10) ? "0" + minute : minute; //set final minute vale
+    second = (second < 10) ? "0" + second : second; // set final second value
+    let time = hour + ":" + minute + ":" + second + " " + period; //set time value
+    document.getElementById("clock").innerText = time; //write time
+    let t = setTimeout(function(){ currentTime(); }, 1000); //output
+}
+currentTime(); //allows function to be called in HTML
